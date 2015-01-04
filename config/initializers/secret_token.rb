6a0a4ef7ed02cf183533d7b6d6a8f9eda5b0e707
EduRivera <eduardriveramestre@gmail.com>
2015-01-04 15:48:02 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smallcommercesabadell::Application.config.secret_token = '7a94b5a28f30c8565535a0b5c35c7594e96d708965de52b15eab7b0d95e20b95b7c0270a444918f85282c323766b1a28bd86eabea010d8fe6371c0a4afb72810'
